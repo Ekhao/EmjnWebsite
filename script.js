188092cc2fe9f761681a373137b98811794b4124
Emil Njor <emil.njor@gmail.com>
2022-04-21 14:37:04 +0200

    naive way to achieve https addresses for every section

--- script.js
+++
@@ -1,7 +0,0 @@
-function showSection(id){
-    const sections = document.getElementsByTagName("section")
-    for(let i = 0; i < sections.length; i++){
-        sections[i].style.display = "none";
-    }
-    document.getElementById(id).style.display = "block";
-}
